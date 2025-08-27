3449dfd4aa151bd8be059c8e721afa74ec40ae23
つづこ <>
2025-08-27 21:32:35 +0900

    モデル、コントローラー、ルーティングの編集

--- db/migrate/20250827122003_add_index_on_reservations_starts_at.rb
+++ db/migrate/20250827122003_add_index_on_reservations_starts_at.rb
@@ -1,4 +1,5 @@
 class AddIndexOnReservationsStartsAt < ActiveRecord::Migration[7.2]
   def change
+    add_index :reservations, :starts_at, unique: true
   end
 end
---
+++ test/controllers/reservations_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class ReservationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get reservations_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get reservations_create_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get reservations_index_url
+    assert_response :success
+  end
+
+  test "should get slots" do
+    get reservations_slots_url
+    assert_response :success
+  end
+end
